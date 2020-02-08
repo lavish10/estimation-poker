bc6a2e6901ccc4078457ad2446da500a1e6acb8e
Lavish Yadav <lavishyadav@live.com>
2020-02-08 16:06:55 +0530

    Added AppBar in Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./containers/HomePage";
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import {blueGrey, deepOrange} from '@material-ui/core/colors';
 
+const theme = createMuiTheme({
+    palette: {
+        primary: blueGrey,
+        secondary: deepOrange,
+    }
+});
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ThemeProvider theme={theme}>
+                <HomePage/>
+        </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Slide from '@material-ui/core/Slide';
+import {Button, createStyles, IconButton, Theme} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import {makeStyles} from "@material-ui/core/styles";
+
+interface Props {
+    window?: () => Window;
+    children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+    const {children, window} = props;
+    const trigger = useScrollTrigger({target: window ? window() : undefined});
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+    }),
+);
+
+export default function Header(props: Props) {
+    const classes = useStyles();
+    return (
+        <>
+            <CssBaseline/>
+            <HideOnScroll {...props}>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography className={classes.title} variant="h6">
+                            Poker
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar/>
+        </>
+    );
+}
