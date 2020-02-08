73d290195133b6ec83f54344376c01a979cc12e4
Lavish Yadav <lavishyadav@live.com>
2020-02-08 22:41:38 +0530

    Added NavTab component

--- src/containers/PageContent.tsx
+++ src/containers/PageContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {createStyles, Grid, Paper, Theme, Typography} from "@material-ui/core";
+import {AppBar, Card, CardHeader, createStyles, Grid, Tab, Tabs, Theme,} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import NavTab from "../components/NavTab";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -12,22 +13,28 @@ const useStyles = makeStyles((theme: Theme) =>
             // textAlign: 'center',
             color: theme.palette.text.secondary,
         },
+        selector: {
+            padding: theme.spacing(2),
+            color: theme.palette.primary.main,
+        }
     }),
 );
 export default function PageContent() {
     let classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
     return (
         <>
-
             <div className={classes.root}>
                 <Grid container spacing={10}>
                     <Grid item xs={12}>
-                        <Paper className={classes.paper}>
-                            <Typography variant={"h5"} color={"secondary"}>
-                                Scrum Poker
-                            </Typography>
-
-                        </Paper>
+                        <Card className={classes.paper}>
+                            <CardHeader title={"Scrum Poker"}/>
+                            <NavTab values={["Create Session", "Join Session"]} tabpanel={[<>Create Session</>, <>Join Session</>]}/>
+                        </Card>
                     </Grid>
                 </Grid>
             </div>
