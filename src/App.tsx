b244b4236661ce1c13e2a465303c654ddb387a93
Lavish Yadav <lavishyadav@live.com>
2020-02-13 21:21:07 +0530

    Add. React Router for HomePage and PokerTable

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import HomePage from "./containers/HomePage";
 import {ThemeProvider} from '@material-ui/core/styles';
 import theme from "./models/MaterialTheme";
+import {HashRouter as Router, Route} from 'react-router-dom';
+import PokerTable from "./containers/PokerTable";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-        <ThemeProvider theme={theme}>
-                <HomePage/>
-        </ThemeProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Route exact path={"/"} component={HomePage}/>
+                    <Route exact path={"/poker-table"} component={PokerTable}/>
+                </Router>
+            </ThemeProvider>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/containers/PokerTable.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import {Card, CardHeader, createStyles, Grid, Paper, Theme} from "@material-ui/core";
+import Header from "../uicomponents/Header";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        paper: {
+            padding: theme.spacing(1),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+        },
+        selector: {
+            padding: theme.spacing(1),
+            color: theme.palette.primary.main,
+        }
+    }),
+);
+export default function PokerTable() {
+    const classes = useStyles();
+    return (
+        <>
+            <Header children={<></>}/>
+            <Container>
+                <Box my={2}>
+                    <>
+                        <div className={classes.root}>
+                            <Grid container spacing={10}>
+                                <Grid item xs={12}>
+                                    <Card className={classes.paper}>
+                                        <CardHeader title={"Poker Table"}/>
+                                        <Grid container spacing={3} direction="row" justify="center"
+                                              alignItems="center">
+                                            <Grid item xs={12} sm={6}>
+                                                <Paper variant={"outlined"} className={classes.paper}>
+                                                </Paper>
+                                            </Grid>
+                                        </Grid>
+                                    </Card>
+                                </Grid>
+
+                            </Grid>
+                        </div>
+                    </>
+                </Box>
+            </Container>
+        </>
+    );
+
+}
